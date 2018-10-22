3cff67bfeb364be05c8753cddb059e59d6a9c2ab
Yggdrasil09 <karthik99th.tnk@gamil.com>
2018-10-22 16:14:47 +0530

    Basic start

---
+++ pendulum.js
@@ -0,0 +1,11 @@
+function setup()
+{
+    var canvas=createCanvas(600, 600);
+    background(0);
+    canvas.position((windowWidth-width)/2,(windowHeight-height)/2);
+}
+
+function draw()
+{
+    
+}
